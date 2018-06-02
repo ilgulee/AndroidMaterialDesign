666f626dd4cb534f7ccc8604529ba0c06433bde4
Ilgu Lee <iglee2000ca@gmail.com>
2018-06-02 19:43:19 -0400

    Create Menus (Components) in ToolBar (main menu)

--- app/src/main/java/ilgulee/com/materialdesignset/MainActivity.java
+++ app/src/main/java/ilgulee/com/materialdesignset/MainActivity.java
@@ -1,7 +1,10 @@
 package ilgulee.com.materialdesignset;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.item1_id){
+            Toast.makeText(getApplicationContext(),"item1 is selected",Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.item2_id){
+            Toast.makeText(getApplicationContext(),"item2 is selected",Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.item3_id){
+            Toast.makeText(getApplicationContext(),"item3 is selected",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
